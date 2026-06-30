8df5ecda31b04a00303427bd84c81bd9b74f4df4
Higor <higordamascenostella@gmail.com>
2026-06-30 15:29:34 -0300

    :sparkles: Loja feitaV1.0

--- lojaDeTenis/src/lojaDeTenis/controller/LojaController.java
+++ lojaDeTenis/src/lojaDeTenis/controller/LojaController.java
@@ -1,5 +1,73 @@
 package lojaDeTenis.controller;
 
-public class LojaController {
+import java.util.ArrayList;
+import java.util.List;
+
+import lojaDeTenis.Repository.LojaRepository;
+import lojaDeTenis.model.Produto;
+
+public class LojaController implements LojaRepository {
+	private int proximoId = 1;
+	private List<Produto> produtos = new ArrayList<>();
+
+	@Override
+	public void listarTodas() {
+		if (produtos.isEmpty()) {
+			System.out.println("Nenhum produto cadastrado");
+			return;
+		}
+		for (Produto p : produtos) {
+			p.visualizar();
+
+		}
+
+	}
+
+	@Override
+	public void procurarPorId(int id) {
+		for (Produto p : produtos) {
+			if (p.getId() == id) {
+				p.visualizar();
+				return;
+			}
+		}
+		System.out.println("Produto não encontrado.");
+
+	}
+
+	@Override
+	public void cadastrar(Produto produto) {
+		produto.setId(proximoId);
+	    proximoId++;
+		produtos.add(produto);
+		System.out.println("Produto cadastrado com sucesso!");
+
+	}
+
+	@Override
+	public void atualizar(Produto produto) {
+		for (int i = 0; i < produtos.size(); i++) {
+			if (produtos.get(i).getId() == produto.getId()) {
+				produtos.set(i, produto);
+				System.out.println("Produto atualizado com sucesso!");
+				return;
+			}
+		}
+		System.out.println("Produto não encontrado.");
+
+	}
+
+	@Override
+	public void deletar(int id) {
+		for (int i = 0; i < produtos.size(); i++) {
+			if (produtos.get(i).getId() == id) {
+				produtos.remove(i);
+				System.out.println("Produto deletado com sucesso!");
+				return;
+			}
+		}
+		System.out.println("Produto não encontrado.");
+
+	}
 
 }
--- lojaDeTenis/src/lojaDeTenis/menu/Menu.java
+++ lojaDeTenis/src/lojaDeTenis/menu/Menu.java
@@ -3,31 +3,32 @@ package lojaDeTenis.menu;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import lojaDeTenis.controller.LojaController;
+import lojaDeTenis.model.Meias;
+import lojaDeTenis.model.Produto;
+import lojaDeTenis.model.Tenis;
+
 public class Menu {
-	
+
 	private static Scanner leia = new Scanner(System.in);
+	private static LojaController controller = new LojaController();
+
 	public static void main(String[] args) {
-		
+
 		int opcao;
 		boolean finalizar = false;
-
 		while (finalizar == false) {
-
 			System.out.println("******************************************************");
 			System.out.println("  \n 		LOJA DE TENIS NIKO						\n");
 			System.out.println("******************************************************");
-			System.out.println("******************************************************");
-			System.out.println("								 				   	  ");
 			System.out.println("  1 - Cadastrar Produto		     					  ");
 			System.out.println("  2 - Listar todos os Produtos	 				  	  ");
 			System.out.println("  3 - Listar Produtos pelo ID	 					  ");
 			System.out.println("  4 - Atualizar Produto								  ");
 			System.out.println("  5 - Deletar Produto			  					  ");
-			System.out.println("  								  					  ");
 			System.out.println("  0 - Sair						 					  ");
 			System.out.println("******************************************************");
 			System.out.println("Entre com a opção desejada:		  					  ");
-
 			try {
 				opcao = leia.nextInt();
 				leia.nextLine();
@@ -36,46 +37,118 @@ public class Menu {
 				System.out.println("Digite um número inteiro entre 0 e 8");
 				leia.nextLine();
 			}
-			
-			switch (opcao){
-				
+
+			switch (opcao) {
+
 			case 1:
-				System.out.println("Cadastrar");
+				try {
+					
+					System.out.println("Digite o tamanho:");
+					int numero = leia.nextInt();
+					leia.nextLine();
+
+					System.out.println("Digite o preço:");
+					String precoTexto = leia.nextLine();
+					float preco = Float.parseFloat(precoTexto.replace(",", "."));
+
+					System.out.println("Escolha o tipo: 1-Tênis  2-Meia");
+					int tipoEscolhido = leia.nextInt();
+					leia.nextLine();
+
+					Produto produto;
+					if (tipoEscolhido == 1) {
+						produto = new Tenis(0, numero, preco);
+					} else {
+						produto = new Meias(0, numero, preco);
+					}
+
+					controller.cadastrar(produto);
+
+				} catch (InputMismatchException e) {
+					System.out.println("Erro: digite valores válidos.");
+					leia.nextLine();
+				}
 				KeyPress();
 				break;
-				
+
 			case 2:
-				System.out.println("Listar Todos");
+				controller.listarTodas();
 				KeyPress();
 				break;
-				
+
 			case 3:
-				System.out.println("Listar prod");
+				try {
+					System.out.println("Digite o ID do produto:");
+					int id = leia.nextInt();
+					leia.nextLine();
+					controller.procurarPorId(id);
+				} catch (InputMismatchException e) {
+					System.out.println("ID inválido.");
+					leia.nextLine();
+				}
 				KeyPress();
 				break;
-				
+
 			case 4:
-				System.out.println("Atualizar");
+				try {
+
+					System.out.println("Digite o ID do produto a atualizar:");
+			        int id = leia.nextInt();
+			        leia.nextLine();
+			        
+					System.out.println("Digite o novo tamanho:");
+					int numero = leia.nextInt();
+					leia.nextLine();
+
+					System.out.println("Digite o preço:");
+					String precoTexto = leia.nextLine();
+					float preco = Float.parseFloat(precoTexto.replace(",", "."));
+
+					System.out.println("Escolha o tipo: 1-Tênis  2-Meia");
+					int tipoEscolhido = leia.nextInt();
+					leia.nextLine();
+
+					Produto produto;
+					if (tipoEscolhido == 1) {
+						produto = new Tenis(id, numero, preco);
+					} else {
+						produto = new Meias(id, numero, preco);
+					}
+
+					controller.atualizar(produto);
+
+				} catch (InputMismatchException e) {
+					System.out.println("Erro: digite valores válidos.");
+					leia.nextLine();
+				}
 				KeyPress();
 				break;
-				
+
 			case 5:
-				System.out.println("Deletar");
+				try {
+					System.out.println("Digite o ID do produto a deletar:");
+					int id = leia.nextInt();
+					leia.nextLine();
+					controller.deletar(id);
+				} catch (InputMismatchException e) {
+					System.out.println("ID inválido.");
+					leia.nextLine();
+				}
 				KeyPress();
 				break;
-				
+
 			case 0:
 				System.out.println("Sistema finalizado");
 				finalizar = true;
-				
 				break;
-				
+
 			}
 		}
-		
+
 	}
+
 	public static void KeyPress() {
-		System.out.println( "\n\nPressione Enter para continuar...");
+		System.out.println("\n\nPressione Enter para continuar...");
 		leia.nextLine();
 	}
 }
